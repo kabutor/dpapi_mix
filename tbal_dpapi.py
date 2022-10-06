fbcb68d970c41c54f69415c9ea183826afe3219c
kabutor <43006263+kabutor@users.noreply.github.com>
2022-10-06 15:54:55 +0200

    TBAL fixes, also renamed file

--- tbal_dpapi.py
+++ tbal_dpapi.py
@@ -20,28 +20,34 @@ from optparse import OptionParser
 
 
 parser = OptionParser()
-parser.add_option("--system", metavar="HIVE", dest="system", default=os.path.join('Windows','System32','config','SYSTEM'))
-parser.add_option("--security", metavar="HIVE", dest="security", default=os.path.join('Windows','System32','config','SECURITY'))
+parser.add_option("--dir", metavar="NAME", dest="dir_win", help="Windows directory, just need this and try to find the rest.")
+parser.add_option("--system", metavar="HIVE", dest="system", help=(' SYSTEM file in Windows\System32\config\SYSTEM'))
+parser.add_option("--security", metavar="HIVE", dest="security", help = ('SECURITY file in Windows\System32\config\SECURITY'))
 parser.add_option("--mkdir", metavar="NAME", dest="mkdir", help="Masterkey Directory Location")
 parser.add_option("--sid", metavar="NAME", dest="sid_value", help="manually specify SID")
-
 (options, args) = parser.parse_args()
+
+if options.dir_win:
+    options.system = os.path.join(options.dir_win, 'System32', 'config','SYSTEM')
+    options.security = os.path.join(options.dir_win, 'System32', 'config','SECURITY')
+if ((options.system == None) or (options.security == None)):
+    parser.print_help()
+    sys.exit(0)
 # read LSA secrets, find TBAL key
 reg = registry.Regedit()
 secrets = reg.get_lsa_secrets(options.security, options.system)
 for i in list(secrets.keys()):
     for k, v in list(secrets[i].items()):
         if k in ('CurrVal', 'OldVal'):
-            ## String
-            #data = v
-            try: data = v.decode('utf-16le')
-            except:
-                data = v.hex()
-            #print(('\t'.join([i, k, '\t'+str(data)])))
+            #try: data = v.decode('utf-16le')
+            data = v.hex()
             if ("TBAL" in i):
                 ntlm = str(data)[32:64]
                 tbal_data = str(data)[96:136]
                 print ('NTLM: %s  DPAPI_key %s' % (ntlm, tbal_data))
+                try: print('User: %s' % binascii.unhexlify((data)[288:]).decode())
+                except:
+                    pass
                 break
 if (tbal_data) == None:    
     sys.exit("No TBAL found")
